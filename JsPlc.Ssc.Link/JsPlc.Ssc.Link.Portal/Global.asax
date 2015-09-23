beeec30056e4cfeb4fa278d28208112378a22eba
luanatsainsburys <Luan.Au@sainsburys.co.uk>
2015-09-23 16:57:37 +0100

    Initial solution

---
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JsPlc.Ssc.Link.Portal.MvcApplication" Language="C#" %>
---
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JsPlc.Ssc.Link.Portal.Startup))]
+namespace JsPlc.Ssc.Link.Portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
