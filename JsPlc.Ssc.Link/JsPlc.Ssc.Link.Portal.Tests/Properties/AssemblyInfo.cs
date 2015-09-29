eae3d2c12c86f4f912eec15eeb27f848eb47aaaf
luanatsainsburys <Luan.Au@sainsburys.co.uk>
2015-09-29 10:05:43 +0100

    New 4.5 portal projects

--- JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal.Tests/Properties/AssemblyInfo.cs
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JsPlc.Ssc.Link.Portal")]
+[assembly: AssemblyTitle("JsPlc.Ssc.Link.Portal.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JsPlc.Ssc.Link.Portal")]
+[assembly: AssemblyProduct("JsPlc.Ssc.Link.Portal.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c6be6515-fe21-4bdc-95fb-556706f33762")]
+[assembly: Guid("ca4b28be-88ae-4adb-87b0-6af419346799")]
 
 // Version information for an assembly consists of the following four values:
 //
