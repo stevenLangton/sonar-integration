7f1191d7769186cf02103fb56c631fe3373852db
luanatsainsburys <luan.au@sainsburys.co.uk>
2015-10-02 09:01:17 +0100

    LINK-26 Initial solution to pdf download function

---
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Controllers/PdfController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.IO;
+using iTextSharp.text.pdf;
+using System.Configuration;
+using System.Web.Configuration;
+
+namespace JsPlc.Ssc.Link.Portal.Controllers
+{
+    public class PdfController : Controller
+    {
+        // GET: Pdf
+        public ActionResult New()
+        {
+            String MimeTypeStr = MimeMapping.GetMimeMapping("*.pdf");
+
+            MemoryStream newFileStream = new MemoryStream();
+
+            var AppSettings = ConfigurationManager.AppSettings;
+
+            var MeetingTemplateFileName = AppSettings["MeetingTemplateFileName"]??@"\PdfTemplates\MeetingTemplate.pdf";
+
+            var TemplateFileName = HttpContext.Server.MapPath(MeetingTemplateFileName);
+
+            MakeMeetingPdf(newFileStream, TemplateFileName);
+
+            return File(newFileStream.GetBuffer(), MimeTypeStr, "Meeting.pdf");
+        }
+
+        public static MemoryStream MakeMeetingPdf(MemoryStream newFileStream, string fileNameExisting)
+        {
+            using (var existingFileStream = new FileStream(fileNameExisting, FileMode.Open))
+            {
+                // Open existing PDF template
+                var pdfReader = new PdfReader(existingFileStream);
+
+                // PdfStamper, which will create new pdf
+                var stamper = new PdfStamper(pdfReader, newFileStream);
+
+                var form = stamper.AcroFields;
+                var fieldKeys = form.Fields.Keys;
+
+                foreach (string fieldKey in fieldKeys)
+                {
+                    form.SetField(fieldKey, "Diesel Scrum!State-of-the art colleagues appraisal system.");
+                }
+
+                // "Flatten" the form so it wont be editable/usable anymore
+                stamper.FormFlattening = true;
+
+                // You can also specify fields to be flattened, which
+                // leaves the rest of the form still be editable/usable
+                stamper.PartialFormFlattening("field1");
+
+                stamper.Close();
+                pdfReader.Close();
+            }
+
+            return newFileStream;
+        }
+    }
+}
+
+
