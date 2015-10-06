b1496f67bf875e6929014e6e859e1fb55375e7a1
luanatsainsburys <Luan.Au@sainsburys.co.uk>
2015-10-06 16:55:55 +0100

    Prepare for Team page

---
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Scripts/App/kocomponents/RegisterKoComponents.js
@@ -0,0 +1,42 @@
+﻿define(['jquery', 'knockout', 'ko-binding-handlers', 'text'], function ($, ko) {
+    //knockout component registrations
+    ko.components.register("location",
+    {
+        require: "kocomponents/location"
+    });
+    ko.components.register("location-heirarchy",
+    {
+        require: "kocomponents/locationHierarchy"
+    });
+
+    ko.components.register("product-level",
+    {
+        require: "kocomponents/productLevel"
+    });
+
+    ko.components.register("product-heirarchy",
+    {
+        require: "kocomponents/productHierarchy"
+    });
+
+    ko.components.register("store-grades",
+    {
+        require: "kocomponents/storeGrades"
+    });
+
+    ko.components.register("alerts-filter",
+    {
+        require: "kocomponents/alertsFilter"
+    });
+
+    ko.components.register("products-filter",
+    {
+        require: "kocomponents/productsFilter"
+    });
+
+    ko.components.register("login-panel",
+    {
+        require: "kocomponents/loginPanel"
+    });
+
+});//Module end
