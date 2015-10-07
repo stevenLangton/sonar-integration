382a5c1c49e0e865a5275b3dda4b6235f0be67da
luanatsainsburys <Luan.Au@sainsburys.co.uk>
2015-10-07 14:36:13 +0100

    Add method to create meeting pdf from  json  meeting data

--- JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Controllers/PdfController.cs
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Controllers/PdfController.cs
@@ -9,33 +9,62 @@ using iTextSharp.text.pdf;
 using System.Configuration;
 using System.Web.Configuration;
 using JsPlc.Ssc.Link.Models;
+using System.Collections.Specialized;
 
 namespace JsPlc.Ssc.Link.Portal.Controllers
 {
     public class PdfController : Controller
     {
         private Lazy<LinkServiceFacade> _LinkService;
+        private string AppBasePath { get; set; }
+        private NameValueCollection AppSettings { get; set; }
 
         public PdfController()
         {
             _LinkService = new Lazy<LinkServiceFacade>();
+
+            AppSettings = ConfigurationManager.AppSettings;
         }
 
         // GET: Pdf
-        public ActionResult New(int MeetingId)
+        public ActionResult MakeFromDb(int MeetingId)
         {
-            var AppBasePath = Request.ApplicationPath;
-            var AppSettings = ConfigurationManager.AppSettings;
+            AppBasePath = Request.ApplicationPath;
+            //var AppSettings = ConfigurationManager.AppSettings;
 
-            var MeetingTemplateFileName = AppSettings["MeetingTemplateFileName"]??@"\PdfTemplates\MeetingTemplate.pdf";
+            //var MeetingTemplateFileName = AppSettings["MeetingTemplateFileName"]??@"\PdfTemplates\MeetingTemplate.pdf";
+            //MeetingTemplateFileName = Path.Combine(AppBasePath, MeetingTemplateFileName);
+            //MeetingTemplateFileName = HttpContext.Server.MapPath(MeetingTemplateFileName);
+
+            //MemoryStream newFileStream = new MemoryStream();
+            //MakeMeetingPdf(newFileStream, GetPdfTemplateFileName(), MeetingId);
+
+            MeetingView MeetingData = _LinkService.Value.GetMeeting(MeetingId);
+
+            PdfMeetingTemplate maker = new PdfMeetingTemplate(MeetingData, GetPdfTemplateFileName());
+
+            String MimeTypeStr = MimeMapping.GetMimeMapping("*.pdf");
+            return File(maker.MakePdf().GetBuffer(), MimeTypeStr, "Meeting.pdf");
+        }
+
+        // GET: Pdf
+        public ActionResult MakeFromJson(MeetingView MeetingData)
+        {
+            AppBasePath = Request.ApplicationPath;
+
+            PdfMeetingTemplate maker = new PdfMeetingTemplate(MeetingData, GetPdfTemplateFileName());
+
+            String MimeTypeStr = MimeMapping.GetMimeMapping("*.pdf");
+            return File(maker.MakePdf().GetBuffer(), MimeTypeStr, "Meeting.pdf");
+        }
+
+        private string GetPdfTemplateFileName()
+        {
+            var MeetingTemplateFileName = AppSettings["MeetingTemplateFileName"] ?? @"\PdfTemplates\MeetingTemplate.pdf";
             MeetingTemplateFileName = Path.Combine(AppBasePath, MeetingTemplateFileName);
             MeetingTemplateFileName = HttpContext.Server.MapPath(MeetingTemplateFileName);
 
-            MemoryStream newFileStream = new MemoryStream();
-            MakeMeetingPdf(newFileStream, MeetingTemplateFileName, MeetingId);
-
-            String MimeTypeStr = MimeMapping.GetMimeMapping("*.pdf");
-            return File(newFileStream.GetBuffer(), MimeTypeStr, "Meeting.pdf");
+            return MeetingTemplateFileName;
         }
 
         public MemoryStream MakeMeetingPdf(MemoryStream newFileStream, string fileNameExisting, int MeetingId)
@@ -125,11 +154,9 @@ namespace JsPlc.Ssc.Link.Portal.Controllers
 
         }//SetPdfFormFields
 
-        #region Experimental
+        #region PDF Factory
         interface ILinkPdfTemplate
         {
-            //string TemplateFileName { get; set; }
-            void SetPdfFormFields(PdfStamper stamper);
             MemoryStream MakePdf();
         }
 
@@ -146,10 +173,34 @@ namespace JsPlc.Ssc.Link.Portal.Controllers
 
             public MemoryStream MakePdf()
             {
-                return new MemoryStream();
+                MemoryStream newFileStream = new MemoryStream();
+
+                using (var existingFileStream = new FileStream(TemplateFileName, FileMode.Open, FileAccess.Read))
+                {
+                    // Open existing PDF template
+                    var pdfReader = new PdfReader(existingFileStream);
+
+                    // PdfStamper, which will create new pdf
+                    var stamper = new PdfStamper(pdfReader, newFileStream);
+
+                    SetPdfFormFields(stamper);
+
+                    // "Flatten" the form so it wont be editable/usable anymore
+                    stamper.FormFlattening = true;
+
+                    // You can also specify fields to be flattened, which
+                    // leaves the rest of the form still be editable/usable
+                    //
+                    //stamper.PartialFormFlattening("field1");
+
+                    stamper.Close();
+                    pdfReader.Close();
+                }
+
+                return newFileStream;
             }
 
-            public void SetPdfFormFields(PdfStamper stamper)
+            private void SetPdfFormFields(PdfStamper stamper)
             {
                 var form = stamper.AcroFields;
                 var fieldKeys = form.Fields.Keys;
