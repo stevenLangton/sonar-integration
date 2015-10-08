f6fb103f834cec661e3d997439b5572d871f9ee8
praveen-kumar-1 <Parveen.kumar@sainsburys.co.uk>
2015-10-08 16:00:04 +0100

    User Identity extended to have FirstName lastname

--- JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Controllers/AccountController.cs
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Controllers/AccountController.cs
@@ -148,7 +148,10 @@ namespace JsPlc.Ssc.Link.Portal.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { 
+                    FirstName =  model.FirstName, LastName = model.LastName,
+                    UserName = model.Email, Email = model.Email };
+                // 
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
