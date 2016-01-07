0708c9c68d75bab0b0e4f2d387ed78366a47ab3b
stevensfarkas <steven.farkas@sainsburys.co.uk>
2016-01-07 16:56:50 +0000

    Added emlah logging to web api

---
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Service/ElmahHandleWebApiErrorAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace JsPlc.Ssc.Link.Service
+{
+    public class ElmahHandleWebApiErrorAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            var e = context.Exception;
+            RaiseErrorSignal(e);
+        }
+
+        private static bool RaiseErrorSignal(Exception e)
+        {
+            var context = HttpContext.Current;
+            if (context == null)
+                return false;
+            var signal = Elmah.ErrorSignal.FromContext(context);
+            if (signal == null)
+                return false;
+            signal.Raise(e, context);
+            return true;
+        }
+    }
+}
