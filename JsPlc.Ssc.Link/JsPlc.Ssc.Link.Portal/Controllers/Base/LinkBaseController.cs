e85cc74e3c45abb4006c65112a7ace79e642355b
luanatsainsburys <Luan.Au@sainsburys.co.uk>
2016-01-07 15:23:52 +0000

    Populate CurrentUser from LinkApi service, not user manager

--- JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Controllers/Base/LinkBaseController.cs
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Controllers/Base/LinkBaseController.cs
@@ -2,8 +2,8 @@
 using JsPlc.Ssc.Link.Models;
 using JsPlc.Ssc.Link.Portal.Helpers.Extensions;
 using JsPlc.Ssc.Link.Portal.Models;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+//using Microsoft.AspNet.Identity;
+//using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace JsPlc.Ssc.Link.Portal.Controllers.Base
 {
@@ -17,18 +17,25 @@ namespace JsPlc.Ssc.Link.Portal.Controllers.Base
 
             if (User == null) return;
 
-            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
-            var appUser = manager.FindByEmail(User.Identity.Name);
-            //var appUser = manager.FindById(User.Identity.GetUserId());
-            if (appUser != null)
+            //var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            //var appUser = manager.FindByEmail(User.Identity.Name);
+            ////var appUser = manager.FindById(User.Identity.GetUserId());
+            //if (appUser != null)
+            //{
+            //    CurrentUser = appUser.ToUserView();
+            //    using (var facade = new LinkServiceFacade())
+            //    {
+            //        CurrentUser.Colleague = facade.GetColleagueByUsername(appUser.UserName);
+            //    }
+            //}
+
+            CurrentUser = new UserView();
+
+            using (var facade = new LinkServiceFacade())
             {
-                CurrentUser = appUser.ToUserView();
-                using (var facade = new LinkServiceFacade())
-                {
-                    CurrentUser.Colleague = facade.GetColleagueByUsername(appUser.UserName);
-                }
+                CurrentUser.IsLineManager = facade.IsManager(User.Identity.Name);
+                CurrentUser.Colleague = facade.GetColleagueByUsername(User.Identity.Name);
             }
-            // populate Colleague data from DB using Api
 
             TempData["CurrentUser"] = CurrentUser; // for UserView object for Views
   
