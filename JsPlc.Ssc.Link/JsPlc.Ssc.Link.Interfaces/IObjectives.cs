59470f7caead976d971f41c9b3f3cc8e6c5c9399
stevensfarkas <steven.farkas@sainsburys.co.uk>
2016-01-12 11:36:50 +0000

    built objectives service

--- JsPlc.Ssc.Link/JsPlc.Ssc.Link.Interfaces/IObjectives.cs
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Interfaces/IObjectives.cs
@@ -1,9 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using JsPlc.Ssc.Link.Models;
 
 namespace JsPlc.Ssc.Link.Interfaces
 {
     public interface IObjectives
     {
+        
+        Objectives GetObjective(int id);
+       
+        bool UpdateObjective(int id, Objectives objectives);
+       
+        bool InsertObjective(Objectives objectives);
+       
+        bool DeleteObjective(int id);
+
+        IEnumerable<Objectives> GetListOfObjectives(int userId, DateTime year);
+        
+        void Dispose();
+        
     }
 }
