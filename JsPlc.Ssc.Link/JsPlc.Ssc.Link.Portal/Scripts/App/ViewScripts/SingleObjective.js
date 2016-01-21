26fd9defeb3c16d3d0567891ef012429034e9e69
luanatsainsburys <Luan.Au@sainsburys.co.uk>
2016-01-21 16:35:40 +0000

    Start to add date formatting

--- JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Scripts/App/ViewScripts/SingleObjective.js
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Scripts/App/ViewScripts/SingleObjective.js
@@ -1,8 +1,9 @@
-﻿define(["jquery", "knockout", "komap"], function ($, ko, komap) {
+﻿define(["jquery", "knockout", "komap", "moment"], function ($, ko, komap, moment) {
     "use strict";
 
     var init = function (serverModel) {
         var vm = komap.fromJS(serverModel);
+        vm.formattedCreateDate = moment(vm.CreateDate);
         ko.applyBindings(vm, document.getElementById('ObjectiveView'));
     };
 
