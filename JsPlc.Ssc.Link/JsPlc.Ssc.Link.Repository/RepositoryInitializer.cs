baaa97a3791ff4eebc39867f59c4a58a6079f8a1
stevensfarkas <steven.farkas@sainsburys.co.uk>
2016-01-25 13:30:23 +0000

    pdp service update

--- JsPlc.Ssc.Link/JsPlc.Ssc.Link.Repository/RepositoryInitializer.cs
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Repository/RepositoryInitializer.cs
@@ -169,7 +169,7 @@ namespace JsPlc.Ssc.Link.Repository
 
             var pdp = new List<LinkPdp>
             {
-                new LinkPdp{Id=1,ColleagueId="E009",ManagerId="E010",signOff=false,achieveObjectives="",achieveObjectivesActions="",achieveObjectivesWhen="",careerAspirations="",careerAspirationsActions="",careerAspirationsWhen="",keyStrengths="",keyStrengthsActions="",keyStrengthsWhen=""}
+                new LinkPdp{Id=1,ColleagueId="E009",signOff=false,achieveObjectives="",achieveObjectivesActions="",achieveObjectivesWhen="",careerAspirations="",careerAspirationsActions="",careerAspirationsWhen="",keyStrengths="",keyStrengthsActions="",keyStrengthsWhen=""}
             };
 
             pdp.ForEach(c => context.Pdp.Add(c));
