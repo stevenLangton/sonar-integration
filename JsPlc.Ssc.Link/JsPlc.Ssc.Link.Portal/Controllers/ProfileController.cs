d2684be9cffd0098bbef0069ffb2e8dcd6adf7ae
luanatsainsburys <Luan.Au@sainsburys.co.uk>
2016-01-26 10:10:50 +0000

    Start on profile page

---
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿using JsPlc.Ssc.Link.Portal.Controllers.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JsPlc.Ssc.Link.Portal.Controllers
+{
+    public class ProfileController : LinkBaseController
+    {
+        // GET: Profile
+        public ActionResult Show(string Id)
+        {
+            return View();
+        }
+    }
+}
