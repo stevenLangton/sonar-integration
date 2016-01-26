94c0d0e7c27a4635e0165079ee39bd619fbabd4c
praveen-kumar-1 <Parveen.kumar@sainsburys.co.uk>
2016-01-26 15:11:40 +0000

    Portal Web.config changes, documented keys + Link Meetings list view

---
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Models/ColleagueTeamView.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace JsPlc.Ssc.Link.Models
+{
+    public class ColleagueTeamView
+    {
+        public ColleagueView Colleague { get; set; }
+        public List<LinkMeetingView> Meetings { get; set; }
+
+        public LinkMeetingView LatestMeeting { get; set; } // For the UI: Current Link Meeting: or Upcoming Meeting: 
+        public LinkMeetingView LastMeeting { get; set; } // For the UI text: Last Meeting: November 16th 2016
+        public int MeetingsInLast12Months { get; set; }
+    }
+}
