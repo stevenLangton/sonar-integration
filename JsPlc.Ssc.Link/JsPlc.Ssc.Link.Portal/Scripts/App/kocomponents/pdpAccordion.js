8f61310df9d10118a532a06e1feae2352adc4a64
luanatsainsburys <Luan.Au@sainsburys.co.uk>
2016-01-28 17:06:56 +0000

    Add skeleton Pdp tab to profile page

---
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Scripts/App/kocomponents/pdpAccordion.js
@@ -0,0 +1,15 @@
+﻿define(["jquery", "text!App/kocomponents/pdpAccordion.html", "underscore", "knockout"], function ($, htmlTemplate, _) {
+    "use strict";
+
+    //View model
+    var viewModel = function (params) {
+        var self = {};
+
+        return self;
+    };
+
+    return {
+        viewModel: viewModel,
+        template: htmlTemplate
+    };
+});
