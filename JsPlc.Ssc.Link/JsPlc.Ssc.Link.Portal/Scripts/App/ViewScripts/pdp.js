6e2cfa349d7d90ea828f6a0b8121612c48e3550e
luanatsainsburys <Luan.Au@sainsburys.co.uk>
2016-01-28 12:18:27 +0000

    Add toastr messaging for pdp modification

---
+++ JsPlc.Ssc.Link/JsPlc.Ssc.Link.Portal/Scripts/App/ViewScripts/pdp.js
@@ -0,0 +1,16 @@
+﻿define(["jquery", "toastr"], function ($, toastr) {
+    "use strict";
+
+    var init = function (pdpUpdated) {
+        if (pdpUpdated) {
+            //toastr to inform successful update
+            toastr.info('Your Pdp was successfully updated');
+        } else {
+            toastr.warning('There was a problem updating your Pdp');
+        }
+    };
+
+    return {
+        init: init
+    };
+});
